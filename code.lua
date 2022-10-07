469ee0da37edeabc7a61d74ecaa0bc71e6251abb
adamchen3 <weiguan.chen@outlook.com>
2022-10-07 21:29:11 +0800

    update

--- code.lua
+++ code.lua
@@ -3,3 +3,11 @@
 CDOTA_BaseNPC:GetPlayerOwnerID() -- 从0开始
 -- js
 Players.GetLocalPlayer() -- 从0开始
+
+--[[获取玩家]]--
+--lua
+PlayerResource:GetPlayer(PlayerID) --> CDOTAPlayerController
+
+--[[获取玩家选择的英雄]]--
+--lua
+PlayerResource:GetSelectedHeroEntity(PlayerID) --> CDOTA_BaseNPC_Hero
